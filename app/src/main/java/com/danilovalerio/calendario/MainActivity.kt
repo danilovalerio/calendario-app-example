e87c2eb1549019bc71835ada4f015e7866173e92
dvalerio <dnotms290>
2019-10-16 22:52:15 -0300

    Customizando a Biblioteca de calendarView e explorando os recursos

--- app/src/main/java/com/danilovalerio/calendario/MainActivity.kt
+++ app/src/main/java/com/danilovalerio/calendario/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.danilovalerio.calendario
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.CalendarView
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,9 +11,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        calendarView.setOnDateChangeListener { calendarView: CalendarView, ano: Int, mes: Int, diaDoMes: Int ->
-            Log.i("CALEND", ano.toString()+"/"+mes+"/"+diaDoMes)
-            textViewDataSelecionada.setText(diaDoMes.toString()+"/"+mes+"/"+ano)
-        }
+        //definir datas específicas
+//        calendarView.state().edit()
+//            .setMinimumDate(CalendarDay.from(2019,0,1))
+//            .setMaximumDate(CalendarDay.from(2019,11,31))
+//            .commit()
+
+        //quando muda o mes exibe o valor
+        calendarView.setOnMonthChangedListener({
+            widget, date ->
+            Log.i("CALEND", date.month.plus(1).toString()+"/"+date.year)
+        })
+
+        //Quando muda data exibe o valor
+//        calendarView.setOnDateChangedListener({
+//            widget, date, selected ->
+//            Log.i("CALEND", date.toString())
+//        })
+
+//        calendarView.setOnDateChangeListener { calendarView: CalendarView, ano: Int, mes: Int, diaDoMes: Int ->
+//            Log.i("CALEND", ano.toString()+"/"+mes+"/"+diaDoMes)
+//            textViewDataSelecionada.setText(diaDoMes.toString()+"/"+mes+"/"+ano)
+//        }
+
+        //definir valores predefinidos para meses
+        val meses = arrayOf<String>("Jan","Fev","Mar","Abril","Mai","Jun","Jul","Ago","Set","Out","Nov","Dez")
+        calendarView.setTitleMonths(meses)
+
+        var semanas = arrayOf<String>("Seg","Ter","Qua","Qui","Sex","Sáb","Dom")
+        calendarView.setWeekDayLabels(semanas)
     }
 }
