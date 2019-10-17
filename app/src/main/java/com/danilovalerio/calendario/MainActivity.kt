d73d8ae02f49b9e50150e1ec081667768c22d998
dvalerio <dnotms290>
2019-10-16 22:06:51 -0300

    Ao selecionar uma data no calendário exibi-la no textView logo abaixo do CalendarView

--- app/src/main/java/com/danilovalerio/calendario/MainActivity.kt
+++ app/src/main/java/com/danilovalerio/calendario/MainActivity.kt
@@ -2,11 +2,19 @@ package com.danilovalerio.calendario
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.CalendarView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        calendarView.setOnDateChangeListener { calendarView: CalendarView, ano: Int, mes: Int, diaDoMes: Int ->
+            Log.i("CALEND", ano.toString()+"/"+mes+"/"+diaDoMes)
+            textViewDataSelecionada.setText(diaDoMes.toString()+"/"+mes+"/"+ano)
+        }
     }
 }
